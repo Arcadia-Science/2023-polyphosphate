9bf5248140955b85c1c77a005593815b49888e17
elizabethmcd <elizabethmcd93@gmail.com>
2023-05-25 20:33:29 -0700

    added tet japonica results, not much there

--- scripts/ppk1-seq-vs-structure-comps.R
+++ scripts/ppk1-seq-vs-structure-comps.R
@@ -55,9 +55,8 @@ all_CAP_ppk1_comps_plot <- ppk1_results_metadata %>%
   ggplot(aes(x=seqid, y=alntmscore)) +
   geom_point(aes(color=Phylum), alpha=0.5) + 
   scale_color_manual(values = c("#5088C5", "#F28360", "#3B9886", "#F898AE", "#7A77AB", "#F7B846", "#97CD78", "#BAB0A8", "#C85152", "#8A99AD")) + 
-  geom_rect(xmin = -Inf, xmax = Inf, ymin = 0.97, ymax = Inf, fill = "transparent", color = "black") +
   theme_pubr(legend = c(0.8, 0.5)) +
-  labs(x="Protein Sequence Identity", y="Protein Strucutre Alignment (tm_score)") +
+  labs(x="Protein Sequence Identity", y="Protein Strucutre Alignment (Tm score)") +
   ggtitle("Comparisons of Protein Sequence Identity and Structure Alignment to Candidatus Accumulibacter Ppk1")
 
 all_CAP_ppk1_comps_plot
@@ -76,3 +75,54 @@ top_hits_info <- ppk1_results_metadata %>%
   filter(alntmscore > 0.97) %>% 
   arrange(desc(alntmscore)) %>% 
   select(accession, seqid, alntmscore, Taxonomic.lineage, Phylum)
+
+#################################################
+# Tetrasphaera japonica ppk1 comparisons
+#################################################
+
+# mmseqs easy-search results 
+tet_ppk1_mmseqs <- read.table("results/TET_ppk1_sequences_search.m8", sep="\t", col.names = c("mmseqs_query", "mmseqs_target", "seqid", "alnlen", "mismatch", "gaps", "qstart", "qend", "tstart", "tend", "evalue", "bits"))
+
+# foldseek easy-search results
+tet_ppk1_foldseek <- read.table("results/TET_ppk1_structures_search.m8", sep="\t", col.names=c("foldseek_query","foldseek_target","fident","alnlen","alntmscore","qstart","qend","tstart","tend","evalue","bits"))
+
+# histograms of seq id and alntm
+tet_ppk1_mmseqs %>% 
+  ggplot(aes(x=seqid)) +
+  geom_histogram()
+
+tet_ppk1_foldseek %>% 
+  ggplot(aes(x=alntmscore)) +
+  geom_histogram()
+
+# reorganize and combine by accession
+tet_ppk1_mmseqs_filtered <- tet_ppk1_mmseqs %>% 
+  select(mmseqs_target, seqid) %>% 
+  mutate(accession = mmseqs_target) %>% 
+  select(accession, seqid)
+
+tet_ppk1_foldseek_filtered <-tet_ppk1_foldseek %>% 
+  select(foldseek_target, alntmscore) %>% 
+  mutate(accession = gsub("-F1-model_v1.pdb", "", foldseek_target)) %>% 
+  select(accession, alntmscore)
+
+tet_ppk1_results <- left_join(tet_ppk1_mmseqs_filtered, tet_ppk1_foldseek_filtered)
+
+# merge with metadata to get taxonomy information
+tet_ppk1_results_metadata <- left_join(tet_ppk1_results, ppk1_metadata) %>% 
+  mutate(Phylum = if_else(is.na(Phylum), "Other", Phylum))
+
+# plot comparison of protein seqid to alntmscore against Candidatus Accumulibacter ppk1 query 
+top_tet_filtered_phyla <- tet_ppk1_results_metadata %>% 
+  count(Phylum) %>% 
+  top_n(10, n) %>% 
+  pull(Phylum)
+
+tet_ppk1_results_metadata %>% 
+  mutate(Phylum = if_else(Phylum %in% top_tet_filtered_phyla, Phylum, "Other")) %>% 
+  ggplot(aes(x=seqid, y=alntmscore)) +
+  geom_point(aes(color=Phylum), alpha=0.5) + 
+  scale_color_manual(values = c("#5088C5", "#F28360", "#3B9886", "#F898AE", "#7A77AB", "#F7B846", "#97CD78", "#BAB0A8", "#C85152", "#8A99AD")) + 
+  theme_pubr(legend = c(0.9, 0.4)) +
+  labs(x="Protein Sequence Identity", y="Protein Strucutre Alignment (tm_score)") +
+  ggtitle("Comparisons of Protein Sequence Identity and Structure Alignment to Tetrasphaera japonica Ppk1")
